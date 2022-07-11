35988685451b3249edb6f84ee6e7f84dae13fabe
Ahmed_Diab <ge23miw@mytum.de>
2022-07-11 21:19:20 +0200

    Finished Milestone 1

---
+++ pljit/lib/CodeManager.cpp
@@ -0,0 +1,3 @@
+//
+// Created by a7mediab97 on 11.07.22.
+//
---
+++ pljit/lib/CodeManager.h
@@ -0,0 +1,8 @@
+//
+// Created by a7mediab97 on 11.07.22.
+//
+
+#ifndef PLJIT_CODEMANAGER_H
+#define PLJIT_CODEMANAGER_H
+
+#endif //PLJIT_CODEMANAGER_H
