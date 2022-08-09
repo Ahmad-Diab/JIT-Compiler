feeca00d82c269d109eba1cc1780bef1c99999b0
Ahmed_Diab <ge23miw@mytum.de>
2022-08-09 17:26:00 +0200

    fix lint in AST.h and EvaluationContext.h

--- pljit/AST.h
+++ pljit/AST.h
@@ -5,6 +5,7 @@
 #include "pljit/ParseTree.h"
 #include <cassert>
 #include <unordered_map>
+#include <optional>
 
 namespace jitcompiler {
 //---------------------------------------------------------------------------
--- pljit/EvaluationContext.h
+++ pljit/EvaluationContext.h
@@ -4,6 +4,7 @@
 #include <string_view>
 #include <unordered_map>
 #include <vector>
+#include <optional>
 
 //---------------------------------------------------------------------------
 namespace jitcompiler {
